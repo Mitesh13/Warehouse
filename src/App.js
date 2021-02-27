9ba4f117fd392108b2172ca965837dfd8181769f
Mitesh Chaudhari <mitesh.chaudhari@crestdatasys.com>
2021-02-28 02:10:40 +0530

    2nd try

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Warehouses from './Components/Warehouses'
 import WarehouseView from './Components/WarehouseView'
 import Header from './Components/Header'
-import './app.css'
+import './App.css'
 
 function App() {
 
